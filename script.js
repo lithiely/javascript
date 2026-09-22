7e2c4c64ff0b86cf65e367ab72b2b07752230c60
lithiely <lithielykuruncit@gmail.com>
2026-09-22 15:34:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome="lithiely"
+let idade= 15
+let cidade= "londrina"
+let hobby= volei
+let série= 1
+
+const idadeEmDias = idade * 365
+
+
+console.log("o meu nome é " + nome + "e tenho" + idade + "anos")
+
+console.log(`o meu nome é ${nome} e moro em ${cidade}  tenho ${idade} anos,  minha idade em dias é 
+             ${idadeEmDias} volei ${hobby} 1 ${série}`)
