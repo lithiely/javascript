7f5593f4b7b6350805dc6e8dd436fb487fc24117
lithiely <lithielykuruncit@gmail.com>
2026-09-22 15:41:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ const idadeEmDias = idade * 365
 
 console.log("o meu nome é " + nome + "e tenho" + idade + "anos")
 
-console.log(`o meu nome é ${nome} e moro em ${cidade}  tenho ${idade} anos,  minha idade em dias é 
-             ${idadeEmDias} volei ${hobby} 1 ${série}`)
+console.log(`o meu nome é ${nome} e moro em ${cidade}  tenho ${idade} anos,  minha idade em dias é  ${idadeEmDias}
+             meu hobbie é  ${hobby} estou na  ${série}° serie  `)
